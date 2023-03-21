bd85a113fa84d2a11bcdde55ac4c3efc01dcaf57
muyJason <97745486+JasonIsHydrated@users.noreply.github.com>
2023-03-21 15:31:24 +0100

    Add files via upload

---
+++ src/test/java/de/hhn/se/labswp/buga23spuga/Buga23spugaApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hhn.se.labswp.buga23spuga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Buga23spugaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
