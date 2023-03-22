1540573c0bb7b886f79a9d6fccccb961ef7a4d8d
Tobias <tbergtold@stud.hs-heilbronn.de>
2023-03-22 14:42:40 +0100

    Deleted my project

--- src/main/java/de/hhn/se/labswp/buga23spuga/AppointmentRepository.java
+++
@@ -1,7 +0,0 @@
-package de.hhn.se.labswp.buga23spuga;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface AppointmentRepository extends CrudRepository<Appointment, Integer> {
-
-}
--- src/main/java/de/hhn/se/labswp/buga23spuga/OffersRepository.java
+++
@@ -1,7 +0,0 @@
-package de.hhn.se.labswp.buga23spuga;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface OffersRepository extends CrudRepository<Offer, Integer> {
-
-}
